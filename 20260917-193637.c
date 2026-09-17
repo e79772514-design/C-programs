9c0df28ca848a46386ee60552d3e45b6ff15587f
e79772514-design <e79772514@gmail.com>
2026-09-17 20:08:37 +0300

    Add files via upload

---
+++ 20260917-193637.c
@@ -0,0 +1,23 @@
+/*
+Name:EmmGithinji Gikaru 
+Reg no:CT100/G/30693/26
+Date:17sep2026
+Description:Units offerd this semester 
+*/
+#include <stdio.h>
+int main ()
+{
+printf("1. Foundamentals of digital technology and artificias\n");
+printf ("2. introductin to computer applications\n");
+printf("3. Essentials of accounting\n");
+printf("4. computer systems and organisation\n");
+printf("5. Analogue electronis\n");
+printf("6. Mathematics for science\n");
+printf("7. Introduction to programming and algorithms\n");
+printf("8. Communication and information literacy skills\n");
+
+return 0;
+}
+
+
+    
